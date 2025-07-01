20cf5197fc0189a96f02a688e54fa16cfa247713
AvernusVenine <tatesturm13@gmail.com>
2025-07-01 12:42:25 -0500

    Created layer_to_glacial_codes, along with added saving to csv functionality.

--- glacial_layer_unit_finder.py
+++ glacial_layer_unit_finder.py
@@ -1,20 +1,11 @@
 import pandas as pd
-import geopandas as gpd
-from geopandas import GeoSeries
+import arcpy
 
 geo_data_path = 'gis_data/hennepin/final_grids.gdb'
 
-glacial_layer_names = gpd.list_layers(geo_data_path)
+arcpy.env.workspace = geo_data_path
 
-glacial_units = []
+rasters = arcpy.ListRasters('*')
 
-for _, layer_name in glacial_layer_names.iterrows():
-    name = layer_name['name']
-    name = name.split('_')
-
-    if name[0] not in glacial_units:
-        glacial_units.append(name[0])
-
-with open('glacial_units.txt', 'w') as f:
-    for unit in glacial_units:
-        f.write(unit + ', ')
+for raster in rasters:
+    print(raster)
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import geopandas as gpd
-from geopandas import GeoSeries
-from shapely.geometry import Point, Polygon
-import pyproj
+import arcpy
 
 glacial_units_path = 'glacial_units.txt'
 geo_data_path = 'gis_data/hennepin/final_grids.gdb'
@@ -11,12 +8,13 @@ cwi_well_data_path = 'cwi_data/cwi5.csv'
 
 save_data_path = 'output_data/c5gu.csv'
 
-glacial_layer_names = gpd.list_layers(geo_data_path)
-glacial_layers_data = {}
+arcpy.env.workspace = geo_data_path
 
 # Get each type of glacial unit as there are multiple layers per unit
 with open(glacial_units_path, 'r') as f:
     glacial_units = f.readline().split(', ')
+
+gu_df = pd.DataFrame(columns=['c5st_seq_no', 'relateid', 'glacial_unit', 'percentage', 'depth_top', 'depth_bot'])
 
 cwi_wells = pd.read_csv(cwi_well_data_path, low_memory=False)
 strat_layers = pd.read_csv(cwi_strat_data_path, low_memory=False, on_bad_lines='skip')
@@ -24,26 +22,59 @@ strat_layers = pd.read_csv(cwi_strat_data_path, low_memory=False, on_bad_lines='
 cwi_wells = cwi_wells[cwi_wells['county_c'] == 27]
 strat_layers = strat_layers[strat_layers['relateid'].isin(cwi_wells['relateid'])]
 
-def save_intersect_data(g_unit : str, relate_id : str, g_series : dict):
+def layer_to_glacial_codes(well, layer):
+    point_str = str(well['utme']) + ' ' + str(well['utmn'])
 
-    well_layers = strat_layers[strat_layers['relateid'] == relate_id]
+    for g_unit in glacial_units:
+        result = arcpy.management.GetCellValue(g_unit + '_top', point_str)
 
-    for _, layer in well_layers.iterrows():
-        continue
+        if result.getOutput(0) == 'NoData':
+            continue
 
-for g_unit in glacial_units:
+        gu_top = float(result.getOutput(0))
+        gu_bot = float(arcpy.management.GetCellValue(g_unit + '_base', point_str).getOutput(0))
 
-    g_series = {}
-    g_series['top'] = gpd.read_file(geo_data_path, layer=g_unit + '_top')
-    g_series['depth'] = gpd.read_file(geo_data_path, layer=g_unit + '_depth')
-    g_series['thick'] = gpd.read_file(geo_data_path, layer=g_unit + '_thick')
+        elev = float(well['elevation'])
+        st_top = elev - float(layer['depth_top'])
+        st_bot = elev - float(layer['depth_bot'])
+        st_thick = st_top - st_bot
 
-    for _, well in cwi_wells.iterrows():
-        relate_id = well['relateid']
+        depth_top = min(gu_top, st_top)
+        depth_bot = max(gu_bot, st_bot)
+        intersect_thick = depth_top - depth_bot
 
-        well_point = Point(well['utme'], well['utmn'])
+        if intersect_thick <= 0:
+            continue
 
-        if g_series['top'].intersects(well_point):
-            save_intersect_data(g_unit, relate_id, g_series)
+        percentage = intersect_thick/st_thick
 
-point = Point(549276, 4871856)
+        data = {
+            'c5st_seq_no': layer['c5st_seq_no'],
+            'relateid': well['relateid'],
+            'glacial_unit': g_unit,
+            'percentage': percentage,
+            'depth_top': depth_top,
+            'depth_bot': depth_bot
+        }
+
+        print(data)
+        gu_df.loc[len(gu_df)] = data
+
+temp_iter = 0
+
+for _, well in cwi_wells.iterrows():
+
+    layers = strat_layers[strat_layers['relateid'] == well['relateid']]
+
+    for _, layer in layers.iterrows():
+        layer_to_glacial_codes(well, layer)
+
+        temp_iter += 1
+
+        if temp_iter > 10:
+            break
+
+    if temp_iter > 10:
+        break
+
+gu_df.to_csv(save_data_path, index=False)
